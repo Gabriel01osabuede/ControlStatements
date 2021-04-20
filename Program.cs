bfb816532a35ea24d04f4ea54f8388f1e9e9b874
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2021-04-20 13:52:47 +0100

    second commmit for control statements

--- Program.cs
+++ Program.cs
@@ -6,7 +6,65 @@ namespace controlStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 10;
+            if(num == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else if(num == 20)
+            {
+                Console.WriteLine("number is 20");
+            }
+            else if(num == 30)
+            {
+                Console.WriteLine("number is 30");
+            }
+            else
+            {
+                Console.WriteLine("type in a number");
+            }
+
+            int numb2 = 20;
+            switch(numb2)
+            {
+                case 10:
+                Console.WriteLine("Number is 10");
+                break;
+                case 20:
+                Console.WriteLine("Number is 20");
+                break;
+                case 30:
+                Console.WriteLine("number is 30");
+                break;
+                default:
+                Console.WriteLine("Invalid Number");
+                break;
+            }
+
+            //int a = 10;
+            // while(a < 20)
+            // {
+            //     Console.WriteLine($"value of a : {a}");
+            //     a++; //also a = a + 1
+            // }
+            
+            
+            // do{
+            //     Console.WriteLine($"value of a : {a}");
+            //     a++; //also a = a + 1
+            // }
+            // while(a > 20);
+
+            for(int i =15; i > 0; i-= 5)
+            {
+                Console.WriteLine(i);
+            }
+
+            string[] cars = {"volvo", "bmw", "benz", "ford", "mazda"};
+            foreach(string i in cars)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
